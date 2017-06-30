77ef302be6e2c47067868688e9497bb941c56d8d
ideawu <ideawu@me>
2017-06-30 17:21:37 +0800

    update

--- module/SipRouter.php
+++ module/SipRouter.php
@@ -9,6 +9,31 @@ class SipRouter extends SipModule
 	}
 	
 	function callout($msg){
+		// TODO: 在此进行路由转换
+		
+		// TESTING:
+		if(1){
+			$remote_ip = '127.0.0.1';
+			$remote_port = 5060;
+			$local_ip = $this->engine->local_ip;
+			$local_port = $this->engine->local_port;
+			if($local_ip === '0.0.0.0'){
+				$local_ip = SIP::guess_local_ip($remote_ip);
+			}
+
+			$caller = new SipCallerSession();
+			$caller->local_ip = $local_ip;
+			$caller->local_port = $local_port;
+			$caller->remote_ip = $remote_ip;
+			$caller->remote_port = $remote_port;
+			$caller->uri = "sip:1001@{$local_ip}";
+			$caller->local = "<sip:2005@{$local_ip}>";
+			$caller->remote = "<{$caller->uri}>";
+			$caller->contact = $caller->local;
+
+			return $caller;
+		}
+		
 		return null;
 	}
 	
