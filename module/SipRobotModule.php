bbb6325b8e841b2416c6d5eff330b8a955c74191
ideawu <ideawu@me>
2017-07-01 19:28:59 +0800

    update

--- module/SipRobotModule.php
+++ module/SipRobotModule.php
@@ -1,7 +1,6 @@
 <?php
 /*
-这是一个终端应用的例子。终端应用和远程终端类似，能处理 INVITE 等，但不涉及网络传输。
-此例子收到 INVITE 后建议会话，并定期发送固定的语音。
+这是一个终端应用的例子。终端应用和远程终端代理类似，在 callout() 中返回一个 Session。
 常见的终端应用，如 IVR，录音。这些终端应用可作为模块，也可能被做成真正的远程终端。
 */
 class SipRobotModule extends SipModule
